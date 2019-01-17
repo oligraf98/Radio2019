4c2d7737f8dc47c75c6acbaa2f76deb2d5a311b2
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2019-01-17 09:32:45 -0600

    Quitar funciones
    Se quitaron las funciones de la clase isOn(), y estadoActual(), para que se pueda cambiar la clase Radio por otra sin problema.

--- src/Main.java
+++ src/Main.java
@@ -6,54 +6,64 @@ public class Main {
         Scanner input = new Scanner(System.in);
         String opcion1;
         boolean in = true;
+        boolean encendido = false;
+        String sintonia = "FM";
         do{
-            System.out.println(radio.estadoActual());
             System.out.println("");
             System.out.println("¿Cual de las siguientes acciones desea tomar?\nIngrese el numero de la opcion que quiere(1-6)\n\t1.Incrementar frecuencia\n\t2.Disminuir frecuencia\n\t3.Guardar estacion en favoritos\n\t4.Ir a favoritos\n\t5.Cambiar Fm/Am\n\t6.Encender o apagar la radio\n\t7.Salir del programa ");
             opcion1 = input.nextLine();
 
             switch (opcion1){
                 case "1":
-                    if(!radio.isOn()){
+                    if(!encendido){
                         System.out.println("Por favor encienda la radio antes de usarla.");
                     }else{
-                        radio.subirFrecuencia();
+                        System.out.println("Estacion seleccionada: "+String.format("%.1f",radio.subirFrecuencia())+sintonia);
                     }
                     break;
                 case"2":
-                    if(!radio.isOn()){
+                    if(!encendido){
                         System.out.println("Por favor encienda la radio antes de usarla.");
                     }else{
-                        radio.bajarFrecuencia();
+                        System.out.println("Estacion seleccionada: "+String.format("%.1f",radio.bajarFrecuencia())+sintonia);
                     }
                     break;
                 case"3":
-                    if(!radio.isOn()){
+                    if(!encendido){
                         System.out.println("Por favor encienda la radio antes de usarla.");
                     }else{
                         System.out.println("Elija en que numero quiere guardar la estacion actual (1-12)");
                         String elNum = input.nextLine();
                         radio.setFavorito(Integer.parseInt(elNum));
+                        System.out.println("Se guardo la estacion actual");
                     }
                     break;
                 case"4":
-                    if(!radio.isOn()){
+                    if(!encendido){
                         System.out.println("Por favor encienda la radio antes de usarla.");
                     }else{
                         System.out.println("Elija el numero de estacion (1-12)");
                         String elNum = input.nextLine();
-                        radio.getFavorito(Integer.parseInt(elNum));
+                        System.out.println("Estacion seleccionada: "+String.format("%.1f",radio.getFavorito(Integer.parseInt(elNum)))+sintonia);
                     }
                     break;
                 case"5":
-                    if(!radio.isOn()){
+                    if(!encendido){
                         System.out.println("Por favor encienda la radio antes de usarla.");
                     }else{
-                        radio.cambiarAmFm();
+                        if(radio.cambiarAmFm()){
+                        	sintonia = "FM";
+                        }
+                        else{
+                        	sintonia = "AM";
+                        };
+                        System.out.println("Se cambio a sintonia "+sintonia);
                     }
                     break;
                 case"6":
                     radio.encendidoRadio();
+                    encendido = true;
+                    System.out.println("Se encendio la radio.");
                     break;
                 case"7":
                     System.out.println("Adios!");
